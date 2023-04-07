293cab476b5401e4642ff0606c5a4b156835353c
manish <chaudharymanish684@gmail.com>
2023-04-07 12:53:44 +0530

    my first commit(manish\)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import {Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import Posts from './pages/Posts';
-import Navbar from './components/Navbar';
-import PostDetails from './components/PostDetails';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import Navbar from "./components/Navbar";
+import PostDetails from "./components/PostDetails";
 
 function App() {
   return (
     <div className="App">
+      {/* Hii now i(Manish) am also this project's contributor */}
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/posts' element={<Posts />} />
-        <Route path='/posts/:postId' element={<PostDetails />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:postId" element={<PostDetails />} />
       </Routes>
     </div>
   );
